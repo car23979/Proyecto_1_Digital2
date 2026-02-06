40d698101fa3b1d22d1afc6a025c94a5b36c1360
car23979 <125841463+car23979@users.noreply.github.com>
2026-02-05 20:00:43 -0600

    cambio de variables

--- Proyecto1_Esclavo_SERVO_DC/Proyecto1_Esclavo_SERVO_DC/servo_timer1.c
+++ Proyecto1_Esclavo_SERVO_DC/Proyecto1_Esclavo_SERVO_DC/servo_timer1.c
@@ -10,12 +10,12 @@
 #include <avr/io.h>
 #include "servo_timer1.h"
 
-void Timer1_Servo1_Init(void) {
+void Servo_Init(void) {
 	DDRB |= (1 << PINB1); // PB1 como salida
 	// No se configura el timer completo aquí
 	// Solo se asume que Timer1 ya está configurado en otra libreria
 }
 
-void SERVO1_set(uint16_t pulso) {
+void Servo_SetAngle(uint16_t pulso) {
 	OCR1A = pulso;
 }
