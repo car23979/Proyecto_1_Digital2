a7ed5268ef9d5f7edd6ac672a3069ed5b7371b83
car23979 <125841463+car23979@users.noreply.github.com>
2026-02-15 22:00:07 -0600

    ADC.H
    Se configura la libreria ADC con la funcion de lectura y escritura

---
+++ Esclavo_Sensor_DC/Esclavo_Sensor_DC/ADC.c
@@ -0,0 +1,16 @@
+/*
+ * ADC.c
+ *
+ * Created: 15/02/2026 09:57:35 p. m.
+ *  Author: David Carranza
+ */ 
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <stdint.h>
+
+void ADC_Init(void);
+uint16_t ADC_Read(uint8_t channel);
+
+#endif
