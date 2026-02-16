c71672d61a65ed9ab2567cff8a990ce97cf97880
car23979 <125841463+car23979@users.noreply.github.com>
2026-02-15 22:29:49 -0600

    se configuro main
    se configuro main, en la logica de como va funcionar el sensor

--- Esclavo_Sensor_DC/Esclavo_Sensor_DC/main.c
+++ Esclavo_Sensor_DC/Esclavo_Sensor_DC/main.c
@@ -24,5 +24,46 @@
 
 
 /************************************************************************/
-/*                                                                      */
+/*                               MAIN                                    */
 /************************************************************************/
+
+int	main(void)
+{
+	uint8_t command;
+	uint16_t soil_value;
+	
+	// Inicializadores
+	I2C_Slave_Init(SLAVE_ADDR);
+	ADC_Init();
+	
+	// Configuración de pin de alimentación del sensor
+	DDRB |= (1 << SOIL_POWER_PIN);
+	PORTB &= ~(1 << SOIL_POWER_PIN);	// Apagado inicialmente
+	
+	while(1)
+	{
+		command = I2C_Slave_WaitForCommand();
+		 if (command == CMD_READ_SOIL)
+		 {
+			 // Encender sensor
+			 PORTB |= (1 << SOIL_POWER_PIN)
+			 _delay_ms(50);		// Estabilizar lectura
+			 
+			 soil_value = ADC_Read(0);	// A0
+			 
+			 // Apagar sensor
+			 PORTB &= ~(1 << SOIL_POWER_PIN);
+			 
+			 // Convertir a 8 bits para I2C
+			 uint8_t soil_value = soil_value >> 2;
+			 
+			 // Preparar respuesta
+			 TWDR = soil_8bit;
+			 
+		 }
+		 
+		 // Re-armar TWI
+		 TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT);
+		 
+	}
+}
