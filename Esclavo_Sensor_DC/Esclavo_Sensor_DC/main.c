93b857d7950725afc6f5dd220defe252ae60142b
car23979 <125841463+car23979@users.noreply.github.com>
2026-02-15 21:56:33 -0600

    Se creo proyecto
    Se creo un nuevo proyecto para el esclavo que va controlar el motor DC y el senso de humedad de suelo

---
+++ Esclavo_Sensor_DC/Esclavo_Sensor_DC/main.c
@@ -0,0 +1,18 @@
+/*
+ * Esclavo_Sensor_DC.c
+ *
+ * Created: 15/02/2026 09:55:55 p. m.
+ * Author : Admin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
